4e1b7e3f7904fc6f950063403af456fc77dfff7c
Marhs2 <wjdehddfl12@gmail.com>
2025-01-20 16:51:37 +0900

    뭐 암튼 완성

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,34 @@ const $itemBox = $('.contents');
 const $title = $('h2');
 const $input = $('.form-group .form-control');
 const $searchBtn = $('.form-group .input-group-btn');
-let data;
-let music;
+const $mainMenu = $('#main-menu')
+const $cart = $('.panel-body  i:nth-child(1)')
+let $musciLayer = [...document.querySelectorAll('.product-grid')]
+let $purchase = {}
 
+console.log($cart);
+
+const response = await fetch('./music_data.json');
+const data = await response.json();
+let $music = data.data;
 
 const option = {
     maximumFractionDigits: 4
 }
 
+$title.textContent = "ALL"
+
+$itemBox.innerHTML = ``
+
+$music.sort((a, b) => {
+    const dataA = new Date(a.release)
+    const dataB = new Date(b.release)
+
+    return dataB -  dataA
+})
 
 
-async function load() {
-    try {
-        const response = await fetch('./music_data.json');
-        const data = await response.json();
-        music = data.data; // 직접 music 변수에 저장
-        return music;
-    } catch (error) {
-    }
-}
-
-
-
-async function init() {
-
-
-
-    await load();
+async function Music() {
 
     const noResearch = document.createElement('div');
     noResearch.textContent = "검색된 앨범이 없습니다.";
@@ -39,57 +40,34 @@ async function init() {
     noResearch.style.display = 'none'
     $itemBox.appendChild(noResearch);
 
+    $mainMenu.removeChild($mainMenu.querySelector('li:nth-child(3)'))
 
 
 
-    Object.keys(music).forEach((e) => {
-        const res = music[e]
 
-        let regex = /[^0-9]/g;
-        let price = parseInt(res.price.replace(regex, ""))
-        price = price.toLocaleString('en-US')
+    let array = []
+    $music.forEach((album) => {
 
 
+        if (!array.includes(album.category)) {
+            array.push(album.category)
+            creatTab(album.category)
+        }
         const newMusic = document.createElement('div')
 
         newMusic.classList.add('col-md-2', 'col-sm-2', 'col-xs-2', 'product-grid');
 
-        newMusic.innerHTML = `
-                            <div class="product-items">
-                                    <div class="project-eff">
-                                        <img class="img-responsive" src="images/${res.albumJaketImage}" alt="Time for the moon night">
-                                    </div>
-                                <div class="produ-cost">
-                                    <h5>${res.albumName}</h5>
-                                    <span>
-                                        <i class="fa fa-microphone"> 아티스트</i> 
-                                        <p>${res.artist}(Lovelyz)</p>
-                                    </span>
-                                    <span>
-                                        <i class="fa  fa-calendar"> 발매일</i> 
-
-                                        <p>${res.release}</p>
-                                    </span>
-                                    <span>
-                                        <i class="fa fa-money"> 가격</i>
-                                        <p>₩${price}</p>
-                                    </span>
-                                    <span class="shopbtn">
-                                        <button class="btn btn-default btn-xs">
-                                            <i class="fa fa-shopping-cart"></i> 추가하기 
-                                        </button>
-                                    </span>
-                                </div>
-                            </div>
-        `
+        newMusic.innerHTML = createItemHtml(album);
 
         $itemBox.appendChild(newMusic)
     })
 
-    let save = [...document.querySelectorAll('.product-grid')]
-
 
     function search() {
+
+        let save = [...document.querySelectorAll('.product-grid')]
+
+
         const query = $input.value.trim().toLocaleLowerCase();
         let hasResult = false
         save.forEach((e) => {
@@ -99,18 +77,16 @@ async function init() {
 
             if (query && (title.textContent.toLocaleLowerCase().includes(query) || artistName.textContent.toLocaleLowerCase().includes(query))) {
                 e.style.display = "block";
-                hasResult = true; 
+                hasResult = true;
             } else {
                 e.style.display = "none";
             }
-
-
         })
         noResearch.style.display = hasResult ? "none" : "block";
     }
 
-    $input.addEventListener('keypress', (e) => {
-        e.key === 'Enter' ? search() : '';
+    $input.addEventListener('keyup', (e) => {
+        if (e.key == "Enter") search();
     })
 
     $searchBtn.addEventListener('click', () => {
@@ -119,17 +95,149 @@ async function init() {
 
 
 
+    function creatTab(e) {
+        const newTab = document.createElement('li')
+        newTab.innerHTML = `<a href="#"><i class="fa fa-youtube-play fa-2x"></i> <span>${e}</span></a>`
+        $mainMenu.appendChild(newTab)
+    }
 
-    // let rese = $$('.fa-calendar');
-    // let array = [];
 
 
 }
 
-init()
-load()
+
+async function tabFilter() {
+    const $searchPart = document.querySelector('.text-center')
+    $mainMenu.addEventListener('click', (e) => {
+
+        const $mainMenuTab = $$('#main-menu li:nth-child(n+2)')
+
+        const category = e.target.closest('a')?.querySelector('span').textContent
+        if (!category) return;
+
+        console.log(category);
+
+        $musciLayer = [...document.querySelectorAll('.product-grid')]
+
+        $mainMenuTab.forEach((a) => {
+            a.querySelector('a').classList.remove('active-menu')
+        })
+        e.target.closest('a').classList.add('active-menu')
+
+        $title.textContent = category
 
 
 
 
-$title.textContent = "All"
+
+
+
+        $itemBox.innerHTML =
+            (category === 'ALL' ? $music : $music.filter((music) => music.category === category))
+                .map(res => `<div class="col-md-2 col-sm-2 col-xs-2 product-grid">${createItemHtml(res)}</div>`)
+                .join('');
+
+
+
+
+
+
+
+
+        // $music.forEach((res) => {
+        //     if (category == 'ALL') {
+        //         const newMusic = document.createElement('div')
+
+        //         newMusic.classList.add('col-md-2', 'col-sm-2', 'col-xs-2', 'product-grid');
+
+        //         newMusic.innerHTML = createItemHtml(res);
+
+        //         $itemBox.appendChild(newMusic)
+        //         r    eturn;
+        //     }
+        //     if (category == res.category) {
+        //         const newMusic = document.createElement('div')
+
+        //         newMusic.classList.add('col-md-2', 'col-sm-2', 'col-xs-2', 'product-grid');
+
+        //         newMusic.innerHTML = createItemHtml(res);
+
+        //         $itemBox.appendChild(newMusic)
+
+        //     }
+        // })
+
+    })
+
+
+
+
+
+
+
+}
+
+function addCart() {
+    let amount = {}
+    setTimeout(() => {
+        let btns = $$('.btn-xs');
+        btns.forEach((btn) => {
+            btn.addEventListener('click', (e) => {
+                const $objectName = e.target.parentNode.parentNode.querySelector('h5').textContent
+
+
+
+                if (Object.hasOwn(amount, $objectName)) {
+                    amount[$objectName]++
+                } else {
+                    amount[$objectName] = 1;
+                }
+
+                e.target.textContent = `추가하기 (${amount[$objectName]}개)`;
+                e.target.classList.add("fa", "fa-shopping-cart")
+                e.target.style.padding = '4px 1px'
+
+
+            });
+        });
+    }, 0);
+}
+
+
+
+const createItemHtml = (album) => `
+    <div class="product-items">
+            <div class="project-eff">
+                <img class="img-albumponsive" src="images/${album.albumJaketImage}" alt="Time for the moon night">
+            </div>
+        <div class="produ-cost">
+            <h5>${album.albumName}</h5>
+            <span>
+                <i class="fa fa-microphone"> 아티스트</i> 
+                <p>${album.artist}(Lovelyz)</p>
+            </span>
+            <span>
+                <i class="fa  fa-calendar"> 발매일</i> 
+
+                <p>${album.release}</p>
+            </span>
+            <span>
+                <i class="fa fa-money"> 가격</i>
+                <p>₩${parseInt(album.price.replace(/[^0-9]/g, "")).toLocaleString('en-US')}</p>
+            </span>
+            <span class="shopbtn">
+                <button class="btn btn-default btn-xs">
+                    <i class="fa fa-shopping-cart"></i> 카트담기 
+                </button>
+            </span>
+        </div>
+    </div>
+    `
+addCart()
+tabFilter()
+Music()
+
+
+
+
+
